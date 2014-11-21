e6577c6a3de9c396641953e8c579dcb175828489
fablet <fable@raintechpdx.com>
2014-11-20 18:17:04 -0800

    added large batch of new classes to models

--- Hiking/settings.py
+++ Hiking/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+DATABASE_PATH = os.path.join(BASE_DIR, 'Hiking.db')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hikes',
+    'localflavor',
 )
 
 MIDDLEWARE_CLASSES = (
--- hikes/models.py
+++ hikes/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django import forms
+from localflavor.us.us_states import STATE_CHOICES
+from localflavor.us.forms import USZipCodeField
 
-# Create your models here.
+
+class Hike(models.Model):
+    name = models.CharField(max_length=180, unique=True)
+    type = models.CharField(max_length=128)
+    likes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Location(models.Model):
+    hike = models.ForeignKey(Hike)
+    address = models.CharField(max_length=128)
+    city = models.CharField(max_length=70)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES, null=True, blank=True, default='OR')
+    zipcode = models.USZipCodeField(max_length=5)
+    region = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.address
+
+    def miles_from_user(self):
+        pass
+
+    def hours_from_user(self):
+        pass
+
+
+class Difficulty(models.Model):
+    hike = models.ForeignKey(Hike)
+    difficulty_level = models.CharField(max_length=25)
+    length = models.IntegerField(default=0)
+    elevation = models.IntegerField(default=0)
+
+
+class Hazard(models.Model):
+    hike = models.ForeignKey(Hike)
+    trail_maintenance = models.CharField(max_length=200)
+    weather_effects = models.CharField(max_length=200)
+    user_reports = models.TextField()
+    user_reports_date = models.DateTimeField()
+    known_challenges = models.TextField
+
+
+class Sights(models.Model):
+    hike = models.ForeignKey(Hike)
+    views = models.TextField()
+    wildlife = models.TextField()
+    # hiker_shared = models.ForeignKey(????) need to link user posted reviews, photos, etc
+
+
+class Equipment(models.Model):
+    hike = models.ForeignKey(Hike)
+    trail_map = models.FileField()
+    # recommended_gear = list
+
+
+#start Hiker classes:
+class Hiker(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    home_address = models.CharField(max_length=128)
+    home_city = models.CharField(max_length=70)
+    home_state = models.CharField(max_length=2, choices=STATE_CHOICES, null=True, blank=True, default='OR')
+    home_zipcode = models.USZipCodeField(max_length=5)
+    health_level = models.CharField(max_length=100)
+    avg_walking_pace = models.FloatField(default=2.0)
+
+
+class HikingLog(models.Model):
+    hiker = models.ForeignKey(Hiker)
+    diaries = models.TextField()
+    
+
