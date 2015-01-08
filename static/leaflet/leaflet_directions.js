e04427c886d960d772d25c22e40998ca0309bc1b
fablet <fable@raintechpdx.com>
2015-01-07 19:03:58 -0800

    mapquest modal working with hardcode addressses - next ajax locations

--- static/leaflet/leaflet_directions.js
+++ static/leaflet/leaflet_directions.js
@@ -1,26 +1,54 @@
 // leaflet / mapquest code to produce directions.
-window.onload = function() {
-    var mapLayer = MQ.mapLayer();
 
-    var map = L.map('map', {
-        layers: mapLayer,
-        center: [ 40.731701, -73.993411 ],
-        minZoom: 8,
-        maxZoom: 12,
-        attribution: 'Map data &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>'
-    });
+    $('#mapModal').on('shown.bs.modal', function() {
+            var map,
+                dir;
+            var mapLayer = MQ.mapLayer();
 
-    L.control.layers({
-        'Map': mapLayer,
-        'Satellite': MQ.satelliteLayer(),
-        'Hybrid': MQ.hybridLayer()
-    }).addTo(map);
-}
+            map = L.map('map', {
+                layers: mapLayer,
+                center: [ 40.045049, -105.961737 ],
+                zoom: 9,
+                minZoom: 8,
+                maxZoom: 12,
+                attribution: 'Map data &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>'
+            });
 
+            L.control.layers({
+                'Map': mapLayer,
+                'Satellite': MQ.satelliteLayer(),
+                'Hybrid': MQ.hybridLayer()
+            }).addTo(map);
 
-mapModal.on('shown.bs.modal', function(){
-    setTimeout(function() {
-        map.invalidateSize();
-   }, 1);
-});
+            dir = MQ.routing.directions()
+                .on('success', function(data) {
+                    var legs = data.route.legs,
+                        html = '',
+                        maneuvers,
+                        i;
+
+                    if (legs && legs.length) {
+                        maneuvers = legs[0].maneuvers;
+
+                        for (i=0; i<maneuvers.length; i++) {
+                            html += (i+1) + '. ';
+                            html += maneuvers[i].narrative + '<br />';
+                        }
+
+                        L.DomUtil.get('route-narrative').innerHTML = html;
+                    }
+                });
+
+            dir.route({
+                locations: [
+                    '80217',
+                    'steamboat springs co'
+                ]
+            });
+
+            map.addLayer(MQ.routing.routeLayer({
+                directions: dir,
+                fitBounds: true
+            }));
+        });
 
